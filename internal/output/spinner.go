fc739eabf2729a6c231fa6377f652a5bb5cf89af
Chris Walker <thatamazingweb@gmail.com>
2018-06-11 21:18:42 +0100

    Simple spinner implementation

---
+++ internal/output/spinner.go
@@ -0,0 +1,48 @@
+package output
+
+import (
+	"fmt"
+	"time"
+)
+
+type spinner struct {
+	Prefix   string
+	doneChan chan struct{}
+}
+
+func NewSpinner() spinner {
+	return spinner{
+		doneChan: make(chan struct{}, 1),
+	}
+}
+
+func (s *spinner) Start() {
+	go func() {
+		for {
+			for _, r := range `-\|/` {
+				select {
+				case <-s.doneChan:
+					return
+				default:
+					fmt.Printf("\rGetting pods %c", r)
+					time.Sleep(100 * time.Millisecond)
+				}
+			}
+		}
+	}()
+}
+
+func (s *spinner) Stop() {
+	s.erase()
+	s.doneChan <- struct{}{}
+}
+
+func (s *spinner) erase() {
+	// loop through output, backspacing
+	l := len(s.Prefix) + 1
+	for i := 0; i < l; i++ {
+		for _, c := range []string{"\b", " ", "\b"} {
+			fmt.Printf(c)
+		}
+	}
+}
